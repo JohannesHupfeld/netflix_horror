aebe37571ed17524f192a09c1d13f5b84dfbd2f3
JohannesHupfeld <j.hupfeld@gmail.com>
2020-08-02 17:56:07 -0400

    created initialize method, save method, self.all method

--- lib/netflix_horror/movie.rb
+++ lib/netflix_horror/movie.rb
@@ -1,2 +1,35 @@
 class NetflixHorror::Movie
+    attr_accessor :title, :url, :year, :rating, :adjusted_score, :critic_consensus, :synopsis, :starring, :directed_by   
+    
+    @@all = []
+    
+    def initialize(att_hash)
+       #@title = att_hash[:title],
+       #@url = att_hash[:url],
+       #@year = att_hash[:year],
+       #@rating = att_hash[:rating],
+       #@adjusted_score = att_hash[:adjusted_score],
+       #@critic_consensus = att_hash[:critic_consensus],
+       #@synopsis = att_hash[:synopsis],
+       #@starring = att_hash[:starring],
+       #@directed_by = att_hash[:directed_by]
+        
+        att_hash.each do |key, value|
+            self.send("#{key}=", value)
+        #itterates throught the hash, then calling the method self.send method to whatever our key is then assigns it to a value. 
+        #first key is title and then the value is what we pass through it.
+        end
+        self.save
+    end
+
+    def save
+        @@all << self
+        self
+    end
+
+    def self.all
+        @@all
+    end
+
+    
 end
