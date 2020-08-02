59cf4cb278aa4fb130766b1791e28be7552adff5
JohannesHupfeld <j.hupfeld@gmail.com>
2020-08-01 21:03:49 -0400

    start method, greeting method, menu method, list_movies method

--- lib/netflix_horror/cli.rb
+++ lib/netflix_horror/cli.rb
@@ -1,6 +1,24 @@
 class NetflixHorror::CLI
     
     def start
-        puts "hello"
+        greeting
+        scrape_movies
+        menu
     end 
+
+    def greeting
+        puts "Welcome to the Netflix Horror app!"
+    end
+
+    def scrape_movies
+    end
+
+    def menu
+    end
+
+    def list_movies
+        NetflixHorror:Review.all.each.with_index(1) do |movie,index|
+            puts "#{index}, #{movie.name}"
+        end
+    end
 end
