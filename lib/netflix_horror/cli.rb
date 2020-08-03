6d8d4158d5bcee8e9a602af8106bfbfaec2a1292
JohannesHupfeld <j.hupfeld@gmail.com>
2020-08-03 15:32:36 -0400

    reveresed order in start method to fix issue

--- lib/netflix_horror/cli.rb
+++ lib/netflix_horror/cli.rb
@@ -2,24 +2,24 @@ class NetflixHorror::CLI
     
     def start
         greeting
-        scrape_movies
+        #scrape_movies
         menu 
         NetflixHorror::Scraper.scrape_movies
+        list_movies
     end 
 
     def greeting
         puts "Welcome to the Netflix Horror app!"
     end
-
-    def scrape_movies
+    
+    def list_movies
+        NetflixHorror::Movie.all.each.with_index(1) do |movie,index|
+            puts "#{index}. #{movie.title}"
+        end
     end
 
     def menu
     end
 
-    def list_movies
-        NetflixHorror:Movie.all.each.with_index(1) do |movie,index|
-            puts "#{index}, #{movie.title}"
-        end
-    end
+    
 end 
--- lib/netflix_horror/scraper.rb
+++ lib/netflix_horror/scraper.rb
@@ -1,25 +1,25 @@
 class NetflixHorror::Scraper
 
     def self.scrape_movies
-        index_page = Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/best-netflix-horror-movies-to-watch-right-now/"))
-        
-        array_of_movies = index_page.css("div.countdown-item")
-        
-        array_of_movies.each do |movie_card| 
-            attributes = {
-                title: movie_card.css("div.article_movie_title a")[0].children.text, 
-                url: movie_card.css("div.article_movie_title a")[0].attributes['href'].value,
-                year: movie_card.css("span.start-year").text,
-                rating: movie_card.css("span.tMeterScore").text,
-                adjusted_score: movie_card.css("div.countdown-adjusted-score").text,
-                critic_consensus: movie_card.css("div.critics-consensus").text,
-                synopsis: movie_card.css("div.synopsis").text,
-                starring: movie_card.css("div.cast").text,
-                directed_by: movie_card.css("div.director").text,
-            }
-            movie = NetflixHorror::Movie.new(attributes)
-            binding.pry
-        end
+       index_page = Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/best-netflix-horror-movies-to-watch-right-now/"))
+       
+       array_of_movies = index_page.css("div.countdown-item")
+       
+       array_of_movies.each do |movie_card| 
+           attributes = {
+               title: movie_card.css("div.article_movie_title a")[0].children.text, 
+               url: movie_card.css("div.article_movie_title a")[0].attributes['href'].value,
+               year: movie_card.css("span.start-year").text,
+               rating: movie_card.css("span.tMeterScore").text,
+               adjusted_score: movie_card.css("div.countdown-adjusted-score").text,
+               critic_consensus: movie_card.css("div.critics-consensus").text,
+               synopsis: movie_card.css("div.synopsis").text,
+               starring: movie_card.css("div.cast").text,
+               directed_by: movie_card.css("div.director").text,
+           }
+           movie = NetflixHorror::Movie.new(attributes)
+           
+       end
     end
 
     def scrape_reviews(movie)
