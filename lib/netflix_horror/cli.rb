230aa157ab9d7b3868e41397ab33588e53d36453
JohannesHupfeld <j.hupfeld@gmail.com>
2020-08-04 14:02:45 -0400

    created get movie method and want more info method

--- lib/netflix_horror/cli.rb
+++ lib/netflix_horror/cli.rb
@@ -6,7 +6,8 @@ class NetflixHorror::CLI
         NetflixHorror::Scraper.scrape_movies
         sorted_movies
         list_movies
-        get_movie_method
+        puts "Please select the number of the corresponding movie that you would like more info on:"
+        get_movie_method #asked for input and reported a teaser of the movie
         menu
     end 
 
@@ -25,7 +26,36 @@ class NetflixHorror::CLI
     end
 
     def get_movie_method
+        input = gets.strip
+        index = input.to_i-1
+        if index.between?(0,49) #a string will be -1 
+            movie = @sorted_movies[index]
+            puts "#{movie.title}:"
+            puts "Rotten Tomatoes rated this movie #{movie.rating}"
+            puts "#{movie.critic_consensus}" 
+            want_more_info(movie)
+        elsif input == "exit"
+            #allow this  method to end
+        else
+            puts "Invalid input, please try again..."
+            get_movie_method #recursion
+        end
     end
+
+    def want_more_info(movie)
+        puts "Read more? (Y/N)"
+        input = "nil"
+        until input == "Y" || input == "N"
+            puts "Type Y(yes) or N(no)"
+            input = gets.strip.upcase
+        end
+        if input = "y"
+            #2nd scape
+        else
+            puts "you ended! " 
+        end
+    end
+
 
     def menu
     end
