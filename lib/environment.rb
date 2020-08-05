02bf19bb1234e9de8e6891043115d5087ce7125d
JohannesHupfeld <j.hupfeld@gmail.com>
2020-08-04 21:56:13 -0400

    added colorized

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,7 @@
 require 'pry'
 require 'open-uri'
 require 'nokogiri'
+require 'colorize'
 
 require_relative "netflix_horror/version"
 require_relative "netflix_horror/cli"
--- lib/netflix_horror/cli.rb
+++ lib/netflix_horror/cli.rb
@@ -6,12 +6,12 @@ class NetflixHorror::CLI
         NetflixHorror::Scraper.scrape_movies
         sorted_movies
         list_movies
-        puts "Please select the number of the corresponding movie that you would like more info on:"
+        puts "Please select a number (1-50) of the corresponding movie that you would like more info on or type exit to leave:".colorize(:red)
         get_movie_method #asked for input and reported a teaser of the movie
     end 
 
     def greeting
-        puts "Welcome to the Netflix Horror app!"
+        puts "Welcome to the Netflix Horror app!".colorize(:red)
     end
 
     def sorted_movies
@@ -29,39 +29,45 @@ class NetflixHorror::CLI
         index = input.to_i-1
         if index.between?(0,49) #a string will be -1 
             movie = @sorted_movies[index]
-            puts "#{movie.title} #{movie.year}:"
-            puts "Rotten Tomatoes rated this movie #{movie.rating}"
-            puts "#{movie.synopsis}"
-            puts "#{movie.starring}"
-            puts "#{movie.directed_by}"
-            puts "#{movie.critic_consensus}" 
+            puts "#{movie.title} #{movie.year}:".colorize(:blue)
+            puts ""
+            puts "Rotten Tomatoes rated this movie #{movie.rating}".colorize(:blue)
+            puts ""
+            puts "#{movie.synopsis}".colorize(:blue)
+            puts ""
+            puts "#{movie.starring}".colorize(:blue)
+            puts ""
+            puts "#{movie.directed_by}".colorize(:blue)
+            puts ""
+            puts "#{movie.critic_consensus}".colorize(:blue)
+            puts "" 
             want_more_info(movie)
-            puts "Please select the number of the corresponding movie that you would like more info on or type exit to leave"
+            puts "Please select a number (1-50) of the corresponding movie that you would like more info on or type exit to leave:".colorize(:red)
             get_movie_method
              
         elsif input == "exit"
             #allow this  method to end
         else
-            puts "Invalid input, please try again..."
+            puts "Invalid input, please try again...".colorize(:red)
             get_movie_method #recursion
         end
     end
 
     def want_more_info(movie)
-        puts "Read reviews (Y/N)?"
+        puts "Read reviews (Y/N)?".colorize(:red)
         input = gets.strip.upcase
         until ["Y","N","YES","NO"].include?(input) #input == "Y" || input =="N "
-            puts "Please type Y or N"
+            puts "Please type yes/Y or no/N".colorize(:red)
             input = gets.strip.upcase
         end
         if input == "Y" || input == "YES"
-            puts "... searching for reviews \n\n"
+            puts "... searching for reviews \n\n".colorize(:red)
             NetflixHorror::Scraper.scrape_reviews(movie)
             movie.reviews.each do |review|
-                puts "#{review.author} from the #{review.press} says #{review.quote}.\n\n"
+                puts "#{review.author} from the #{review.press} says #{review.quote}.\n\n".colorize(:blue)
             end
         else
-            puts "you ended!" 
+            puts "Thank you for checking out the Netflix Horror app!".colorize(:red)
         end
     end
     
